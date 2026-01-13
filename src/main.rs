5827855b471ef45be8f94487e94f5468446491f1
PolMauri5 <polmauri5@gmail.com>
2026-01-13 13:33:45 +0100

    feat: main for velocity testing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
 mod adapters;
 mod core;
 
+use std::time::Instant;
+use rust_decimal::Decimal;
+
+use crate::core::{engine::Engine, state::engine_state::EngineState, types::{event::Event, order::Order, side::Side}};
+
 fn main() {
-    println!("Hello, world!");
+    let state = EngineState::new();
+    let mut engine = Engine::new(state);
+
+    let n = 1_000_000;
+    let start = Instant::now();
+
+    for i in 0..n {
+        let event = Event::NewOrder(Order {
+            order_id: i as u64,
+            side: if i % 2 == 0 { Side::Bid } else { Side::Ask },
+            price: Some(Decimal::from(100)),
+            quantity: Decimal::from(1),
+        });
+
+        engine.process(event);
+    }
+
+    let elapsed = start.elapsed();
+    println!(
+        "Processed {} events in {:?} ({:?} per event)",
+        n,
+        elapsed,
+        elapsed / n
+    );
 }
